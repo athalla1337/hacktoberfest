9d22bb8d73320f175153befd66d62b630ef7ce1f
athalla1337 <92833948+athalla1337@users.noreply.github.com>
2021-10-20 02:48:24 +0100

    Create helloworld-athalla1337.py

---
+++ scripts/helloworld-athalla1337.py
@@ -0,0 +1,15 @@
+// LANGUAGE: Python
+// AUTHOR: Athalla1337
+// GITHUB: https://github.com/Athalla1337
+
+logo = """
+            
+############################################################
+# Name           : Athallah Syahru Syahputra               #
+# Author         : A T H A L L A H  T E A M                #
+############################################################
+         ------------------------------------
+                    Hello, World
+         ------------------------------------
+"""
+print(logo)
